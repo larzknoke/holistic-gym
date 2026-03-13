9d67e68a37de612a98519303f6614e60964c977b
Lars Knoke <l.knoke@colorplus.de>
2026-03-13 08:26:56 +0100

    wip

--- components/headers/HeaderGymWip.jsx
+++ components/headers/HeaderGymWip.jsx
@@ -8,12 +8,12 @@ export default function HeaderGymWip() {
   return (
     <header className="relative wrapper !bg-[#edf2fc]">
       <div className="bg-gym-primary p-1 w-full">
-        <div className="container flex flex-row items-center justify-between">
+        <div className="container flex flex-row items-center justify-center md:justify-between">
           <div className="text-white font-bold flex flex-row items-center gap-1">
             <i className="uil uil-exclamation-circle"></i>
             Neueröffnung: Mai 2026
           </div>
-          <div className="flex flex-row gap-5 text-xs">
+          <div className="flex-row gap-5 text-xs hidden md:flex">
             <div className="flex flex-row gap-1">
               <div>
                 <div className="icon !text-white">
@@ -53,8 +53,31 @@ export default function HeaderGymWip() {
           </div>
         </div>
       </div>
-      <nav className="!pt-15 !pb-8 navbar navbar-expand-lg center-logo navbar-dark  bg-gym-dark opacity-100">
-        <div className="container justify-between items-center">
+      <nav className="pt-8 md:pt-12 pb-8 center-logo  bg-gym-dark">
+        <div className="container justify-around md:justify-between items-start md:items-center w-full flex flex-row">
+          <Image
+            className="logo-light w-16 md:w-32 opacity-50"
+            alt="image"
+            src="/assets/gym/humanus-white.svg"
+            width={304}
+            height={67}
+          />
+          <Image
+            className="logo-light w-36 md:w-60"
+            alt="image"
+            src="/assets/gym/gym-logo-v2.svg"
+            width={193}
+            height={150}
+          />
+          <Image
+            className="logo-light w-16 md:w-34 opacity-50"
+            alt="image"
+            src="/assets/gym/milon-white.svg"
+            width={297}
+            height={47}
+          />
+        </div>
+        {/* <div className="container justify-between items-center">
           <div className="flex flex-row w-full justify-between items-center xl:!hidden lg:!hidden">
             <div className="navbar-brand">
               <Link href={`/index`}>
@@ -66,19 +89,7 @@ export default function HeaderGymWip() {
                 />
               </Link>
             </div>
-            <div className="navbar-other !ml-auto">
-              <ul className="navbar-nav flex-row items-center">
-                <li className="nav-item xl:!hidden lg:!hidden">
-                  <button className="hamburger offcanvas-nav-btn">
-                    <span />
-                  </button>
-                </li>
-              </ul>
-              {/* /.navbar-nav */}
-            </div>
-            {/* /.navbar-other */}
           </div>
-          {/* /.flex */}
           <div className="navbar-collapse-wrapper flex flex-row items-center w-full">
             <div className="navbar-collapse offcanvas offcanvas-nav offcanvas-start gap-3">
               <div className="offcanvas-header lg:mx-auto xl:mx-auto order-0 lg:!order-1 lg:!flex lg:!px-[7rem] xl:!order-1 xl:!flex xl:!px-[10rem]">
@@ -88,18 +99,6 @@ export default function HeaderGymWip() {
                   src="/assets/gym/gym-logo-v2.svg"
                   width={193}
                   height={150}
-                />
-                {/* <div className="teaser hover:cursor-pointer hover:shadow-lg hover:translate-y-[-0.25rem] transition-all duration-[0.2s] ease-in-out motion-reduce:transition-none bg-gym-danger text-white text-center px-5 py-2 rounded absolute top-8 right-6 z-[2] text-xl shadow-sm rotate-2 font-bold">
-                  Vorrausichtlicher Start: Mai 2026!
-                  <span className="text-base block">
-                    Profitiere als Gründungsmitglied
-                  </span>
-                </div> */}
-                <button
-                  type="button"
-                  className="btn-close btn-close-white xl:!hidden lg:!hidden"
-                  data-bs-dismiss="offcanvas"
-                  aria-label="Close"
                 />
               </div>
               <div className="w-full order-1 lg:!order-none lg:!flex xl:!order-none xl:!flex">
@@ -128,58 +127,10 @@ export default function HeaderGymWip() {
                   </li>
                 </ul>
               </div>
-              <div className="offcanvas-body xl:!hidden lg:!hidden order-4 !mt-auto">
-                <div className="offcanvas-footer">
-                  <div>
-                    <a
-                      href="mailto:first.last@email.com"
-                      className="link-inverse"
-                    >
-                      info@holistic-gym.de
-                    </a>
-                    <br />
-                    00 (123) 456 78 90 <br />
-                    <nav className="nav social social-white !mt-4">
-                      <a
-                        className="m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]"
-                        href="#"
-                      >
-                        <i className="uil uil-facebook-f before:content-['eae2'] text-[1rem] !text-gym-secondary" />
-                      </a>
-                      <a
-                        className="m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]"
-                        href="#"
-                      >
-                        <i className="uil uil-instagram before:content-['eb9c'] text-[1rem] !text-gym-secondary" />
-                      </a>
-                      <a
-                        className="m-[0_.7rem_0_0] text-[1rem] transition-all duration-[0.2s] ease-in-out translate-y-0 hover:translate-y-[-0.15rem]"
-                        href="#"
-                      >
-                        <i className="uil uil-youtube before:content-['edb5'] text-[1rem] !text-gym-secondary" />
-                      </a>
-                    </nav>
-                    {/* /.social */}
-                  </div>
-                </div>
-              </div>
             </div>
-            {/* <div className="navbar-other lg:!ml-4 xl:!ml-4">
-              <ul className="navbar-nav !flex-row !items-center !ml-auto">
-                <li className="nav-item hidden xl:block lg:block md:block">
-                  <a href="#" className=" btn btn-sm btn-gym">
-                    HolisticApp
-                  </a>
-                </li>
-              </ul>
-            </div> */}
-            {/* /.navbar-collapse */}
           </div>
-          {/* /.navbar-collapse-wrapper */}
-        </div>
-        {/* /.container */}
+        </div> */}
       </nav>
-      {/* /.navbar */}
     </header>
   );
 }
--- components/homes/home-3/HeroWip.jsx
+++ components/homes/home-3/HeroWip.jsx
@@ -13,7 +13,7 @@ export default function HeroWip() {
         <div className="container pt-7 xl:pt-12 lg:pt-12 md:pt-12 pb-8">
           <div className="flex flex-wrap mx-0 items-center">
             <div
-              className="lg:w-6/12 xl:w-6/12 w-full flex-[0_0_auto] max-w-full px-0 !mt-[50px]"
+              className="lg:w-6/12 xl:w-6/12 w-full flex-[0_0_auto] max-w-full px-0 mt-0 md:mt-12"
               data-cues="slideInDown"
               data-group="page-title"
               data-delay={600}
@@ -62,13 +62,7 @@ export default function HeroWip() {
                 >
                   <i className="icn-caret-right !ml-[0.15rem] !relative z-[2] before:content-['\e900'] !text-[calc(1.355rem_+_1.26vw)]" />
                 </a>
-                <figure className="!rounded-md !shadow-lg">
-                  <div className="hover:cursor-pointer hover:shadow-lg hover:translate-y-[-0.25rem] transition-all duration-[0.2s] ease-in-out motion-reduce:transition-none bg-gym-danger text-white text-center px-5 py-2 rounded absolute -top-12 right-4 z-[2] text-xl shadow-sm rotate-2 font-bold">
-                    Vorrausichtlicher Start: Mai 2026!
-                    <span className="text-base block">
-                      Profitiere als Gründungsmitglied
-                    </span>
-                  </div>
+                <figure>
                   <Image
                     className="!rounded-md"
                     // srcSet="/assets/img/photos/about13@2x.jpg 2x"
@@ -77,6 +71,12 @@ export default function HeroWip() {
                     width={590}
                     height={650}
                   />
+                  <div className=" md:absolute mx-6 -mt-6 md:mt-0 -top-12 right-4 hover:cursor-pointer hover:shadow-lg hover:translate-y-[-0.25rem] transition-all duration-[0.2s] ease-in-out motion-reduce:transition-none bg-gym-danger text-white text-center px-5 py-2 rounded z-[2] text-xl shadow-sm rotate-2 font-bold">
+                    Vorrausichtlicher Start: Mai 2026!
+                    <span className="text-base block">
+                      Profitiere als Gründungsmitglied
+                    </span>
+                  </div>
                 </figure>
               </div>
               {/* /div */}
